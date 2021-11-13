85f3042980a73472cd1f7853310c8fa23e91f670
Юлия Долгополова <dogla3@gmail.com>
2021-11-13 02:04:36 +0200

    Lesson8. Долгополова Юля

--- lesson8.py
+++ lesson8.py
@@ -10,17 +10,17 @@
 #
 # Отсортировать данные из файла с помощью данных функций.
 
-# 1. Необходимо написать функцию, которая считает эти данные из файла и возвращала содержимое файла в той же структкру, что и в файле.
-
 import json
 import re
 
+# 1. Необходимо написать функцию, которая считает эти данные из файла. Параметр функции - имя файла.
 def read_json(filename):
     with open(filename, 'r') as file:
         data = json.load(file)
         return data
 filename = 'data.json'
 data = read_json(filename)
+print(data)
 
 
 # 2. Написать функцию сортировки данных по ФАМИЛИИ в поле "name" (у тех у кого она есть).
@@ -28,11 +28,11 @@ data = read_json(filename)
 # Если фамилии нет, то использовать имя, например Euclid.
 
 def sort_surname(data_dict):
-    surname_list = []
-    surname_list.append(data_dict.get("name").split()[-1])
+    surname_list = data_dict["name"].split(" ")[-1]
     return surname_list
 
 sort_by_surname = sorted(data, key=sort_surname)
+print(sort_by_surname)
 
 
 # 3. Написать функцию сортировки по дате смерти из поля "years".
@@ -46,10 +46,13 @@ def sort_date_of_death(data_dict):
     else:
         return int(years[-1])
 sort_death_date = sorted(data, key=sort_date_of_death)
+print(sort_death_date)
 
 
 # 4. Написать функцию сортировки по количеству слов в поле "text".
+
 def sort_count_word(data_dict):
     count_word = len(data_dict["text"].split(" "))
     return count_word
 sorted_count_word = sorted(data, key=sort_count_word)
+print(sorted_count_word)
